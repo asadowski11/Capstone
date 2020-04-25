2a8e95c9f3b6736ccb339731b71ad5d79c1cf6d1
ploiu <curtislanglitz+github@gmail.com>
2020-04-25 16:41:13 -0400

    Dynamic area stuff for directions
    Add more routes for getting directions
    Remove the UI dialog in favor of voice interaction

--- Capstone/Actions/DirectionsAction.cs
+++ Capstone/Actions/DirectionsAction.cs
@@ -1,19 +1,18 @@
-﻿using Capstone.Common;
-using Capstone.Providers;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
-using Windows.UI.Xaml.Controls;
 using System.Web;
-using Windows.Services.Maps;
-using Windows.Devices.Geolocation;
+using Capstone.Common;
 using Capstone.Models;
+using Capstone.Providers;
+using Capstone.SpeechRecognition;
+using Windows.UI.Xaml.Controls;
 
 namespace Capstone.Actions
 {
     public class DirectionsAction : Action
     {
-        
+
         private string DestinationName { get; set; }
         // TODO change to a different type once we know more about the location stuff for objects
         private string StartingPoint { get; set; }
@@ -27,7 +26,7 @@ namespace Capstone.Actions
         {
             // TODO ask for destination name and set it to our destinationName
 
-            
+
         }
         private async Task<string> InputTextDialogAsync(string title)
         {
@@ -55,26 +54,69 @@ namespace Capstone.Actions
         {
             CommandString = CommandString.ToUpper();
             string strDestination = "";
+            string successMessage = "alright, getting directions to {destination}";
             if (CommandString.Contains(" TO "))
             {
                 strDestination = CommandString.Substring(CommandString.IndexOf(" TO ") + 4);
             }
             else
             {
-                strDestination = await InputTextDialogAsync("Destination");
+                // have bob ask the user where they want to go
+                TextToSpeechEngine.SpeakText(this.MediaElement, "Sure, where do you want to go?");
+                // sleep the thread to give bob enough time to speak
+                if (!await SpeechRecognitionManager.RequestListen(this.GetType(), (text) =>
+                {
+                    strDestination = text;
+                    GetDirections(text);
+                }))
+                {
+                    string message = "Sorry, but something went wrong. To get directions, say \"Hey Bob, how do I get to thePlace\"";
+                    TextToSpeechEngine.SpeakText(this.MediaElement, message);
+                    this.ShowMessage(message);
+                }
+                else
+                {
+                    ProvideDirectionsSuccessMessage(strDestination);
+                }
             }
             if (StringUtils.IsNotBlank(strDestination))
             {
-                Dictionary<string, double> coordinates = await LocationProvider.GetLatitudeAndLongitude();
-                double latitude = coordinates["latitude"];
-                double longitude = coordinates["longitude"];
-                Setting setting = StoredProcedures.QuerySettingByName("Map Provider");
-                MapProvider mapProvider = StoredProcedures.QueryMapProvider(setting.GetSelectedOption().DisplayName);
-                string strQuery = mapProvider.BaseURL.ToString();
-                strQuery = strQuery.Replace("{Latitude}", latitude.ToString()).Replace("{Longitude}", longitude.ToString()).Replace("{Destination}", HttpUtility.UrlEncode(strDestination));
-                var uriMap = new Uri(strQuery);
-                var success = await Windows.System.Launcher.LaunchUriAsync(uriMap);
-            }  
+                GetDirections(strDestination);
+                ProvideDirectionsSuccessMessage(strDestination);
+            }
+        }
+        private async void ProvideDirectionsSuccessMessage(string destination)
+        {
+            // show a link to the search 
+            this.ClearArea();
+            var linkElement = new HyperlinkButton();
+            linkElement.Content = $"Directions to {destination.ToLower()}";
+            linkElement.NavigateUri = new Uri(await GetDirectionsLink(destination));
+            linkElement.FontSize = 24;
+            RelativePanel.SetAlignHorizontalCenterWithPanel(linkElement, true);
+            RelativePanel.SetAlignVerticalCenterWithPanel(linkElement, true);
+            this.DynamicArea.Children.Add(linkElement);
+            TextToSpeechEngine.SpeakText(this.MediaElement, $"Alright, getting {linkElement.Content.ToString().ToLower()}");
+        }
+
+        private async void GetDirections(string destination)
+        {
+            string query = await GetDirectionsLink(destination);
+            var uriMap = new Uri(query);
+            var success = await Windows.System.Launcher.LaunchUriAsync(uriMap);
+        }
+
+        private async Task<string> GetDirectionsLink(string destination)
+        {
+            Dictionary<string, double> coordinates = await LocationProvider.GetLatitudeAndLongitude();
+            double latitude = coordinates["latitude"];
+            double longitude = coordinates["longitude"];
+            Setting setting = StoredProcedures.QuerySettingByName("Map Provider");
+            MapProvider mapProvider = StoredProcedures.QueryMapProvider(setting.GetSelectedOption().DisplayName);
+            string query = mapProvider.BaseURL.ToString();
+            query = query.Replace("{Latitude}", latitude.ToString()).Replace("{Longitude}", longitude.ToString()).Replace("{Destination}", HttpUtility.UrlEncode(destination));
+            return query;
+
         }
     }
 }
