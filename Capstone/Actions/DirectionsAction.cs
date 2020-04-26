e3fe05366c0f44438e5ac268cac363492df2d2fd
ploiu <curtislanglitz+github@gmail.com>
2020-04-26 16:12:24 -0400

    fixing a bug caused by location being blocked

--- Capstone/Actions/DirectionsAction.cs
+++ Capstone/Actions/DirectionsAction.cs
@@ -12,7 +12,6 @@ namespace Capstone.Actions
 {
     public class DirectionsAction : Action
     {
-
         private string DestinationName { get; set; }
         // TODO change to a different type once we know more about the location stuff for objects
         private string StartingPoint { get; set; }
@@ -22,39 +21,10 @@ namespace Capstone.Actions
             this.CommandString = CommandString;
         }
 
-        private void getDestinationName()
-        {
-            // TODO ask for destination name and set it to our destinationName
-
-
-        }
-        private async Task<string> InputTextDialogAsync(string title)
-        {
-            TextBox inputTextBox = new TextBox();
-            inputTextBox.AcceptsReturn = false;
-            inputTextBox.Height = 32;
-            ContentDialog dialog = new ContentDialog();
-            dialog.Content = inputTextBox;
-            dialog.Title = title;
-            dialog.IsSecondaryButtonEnabled = true;
-            dialog.PrimaryButtonText = "Ok";
-            dialog.SecondaryButtonText = "Cancel";
-            if (await dialog.ShowAsync() == ContentDialogResult.Primary)
-                return inputTextBox.Text;
-            else
-                return "";
-        }
-
-        private void getStartingPointName()
-        {
-            // TODO ask for starting location name and set it to our startingPoint
-        }
-
         public async override void PerformAction()
         {
             CommandString = CommandString.ToUpper();
             string strDestination = "";
-            string successMessage = "alright, getting directions to {destination}";
             if (CommandString.Contains(" TO "))
             {
                 strDestination = CommandString.Substring(CommandString.IndexOf(" TO ") + 4);
@@ -91,30 +61,48 @@ namespace Capstone.Actions
             this.ClearArea();
             var linkElement = new HyperlinkButton();
             linkElement.Content = $"Directions to {destination.ToLower()}";
-            linkElement.NavigateUri = new Uri(await GetDirectionsLink(destination));
-            linkElement.FontSize = 24;
-            RelativePanel.SetAlignHorizontalCenterWithPanel(linkElement, true);
-            RelativePanel.SetAlignVerticalCenterWithPanel(linkElement, true);
-            this.DynamicArea.Children.Add(linkElement);
-            TextToSpeechEngine.SpeakText(this.MediaElement, $"Alright, getting {linkElement.Content.ToString().ToLower()}");
+            string directionsLink = await GetDirectionsLink(destination);
+            if (directionsLink != null)
+            {
+                linkElement.NavigateUri = new Uri(directionsLink);
+                linkElement.FontSize = 24;
+                RelativePanel.SetAlignHorizontalCenterWithPanel(linkElement, true);
+                RelativePanel.SetAlignVerticalCenterWithPanel(linkElement, true);
+                this.DynamicArea.Children.Add(linkElement);
+                TextToSpeechEngine.SpeakText(this.MediaElement, $"Alright, getting {linkElement.Content.ToString().ToLower()}");
+            }
         }
 
         private async void GetDirections(string destination)
         {
             string query = await GetDirectionsLink(destination);
-            var uriMap = new Uri(query);
-            var success = await Windows.System.Launcher.LaunchUriAsync(uriMap);
+            if (query != null)
+            {
+                var uriMap = new Uri(query);
+                var success = await Windows.System.Launcher.LaunchUriAsync(uriMap);
+            }
         }
 
         private async Task<string> GetDirectionsLink(string destination)
         {
+            string query = null;
             Dictionary<string, double> coordinates = await LocationProvider.GetLatitudeAndLongitude();
-            double latitude = coordinates["latitude"];
-            double longitude = coordinates["longitude"];
-            Setting setting = StoredProcedures.QuerySettingByName("Map Provider");
-            MapProvider mapProvider = StoredProcedures.QueryMapProvider(setting.GetSelectedOption().DisplayName);
-            string query = mapProvider.BaseURL.ToString();
-            query = query.Replace("{Latitude}", latitude.ToString()).Replace("{Longitude}", longitude.ToString()).Replace("{Destination}", HttpUtility.UrlEncode(destination));
+            if (coordinates != null)
+            {
+                double latitude = coordinates["latitude"];
+                double longitude = coordinates["longitude"];
+                Setting setting = StoredProcedures.QuerySettingByName("Map Provider");
+                MapProvider mapProvider = StoredProcedures.QueryMapProvider(setting.GetSelectedOption().DisplayName);
+                query = mapProvider.BaseURL.ToString();
+                query = query.Replace("{Latitude}", latitude.ToString()).Replace("{Longitude}", longitude.ToString()).Replace("{Destination}", HttpUtility.UrlEncode(destination));
+            }
+            else
+            {
+                // have bob tell the user that location information couldn't be retrieved
+                string message = "Sorry, but I couldn't get your location. In order to get you directions to a place, I need your location to use for the starting point.";
+                TextToSpeechEngine.SpeakText(this.MediaElement, message);
+                this.ShowMessage(message);
+            }
             return query;
 
         }
