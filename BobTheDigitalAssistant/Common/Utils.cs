69261504d4e04b96020bdb2eea5f3b640b7abe6e
ploiu <curtislanglitz+github@gmail.com>
2020-04-29 18:17:48 -0400

    fix database setup and made switching between pages faster

---
+++ BobTheDigitalAssistant/Common/Utils.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Threading;
+using Capstone.Models;
+using Windows.UI.Core;
+
+namespace Capstone.Common
+{
+    public static class Utils
+    {
+        public static string JoinEnum(Type e, string joinString)
+        {
+            string joined = "";
+            var enumValues = Enum.GetValues(e);
+            for (int i = 0; i < enumValues.Length; i++)
+            {
+                if (i > 0)
+                {
+                    joined += joinString;
+                }
+                joined += enumValues.GetValue(i).ToString();
+            }
+            return joined;
+        }
+
+        public static async void RunOnMainThread(Action actionToRun)
+        {
+            await Windows.ApplicationModel.Core.CoreApplication.MainView.CoreWindow.Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () => actionToRun.Invoke());
+        }
+
+        public static bool IsListeningSettingEnabled()
+        {
+            Setting voiceRecognitionSetting = StoredProcedures.QuerySettingByName("Voice Activation");
+            SettingOption chosenSetting = voiceRecognitionSetting.GetSelectedOption();
+            return chosenSetting != null && chosenSetting.DisplayName == "Enabled";
+        }
+
+        public static void RunOnSeparateThread(Action action, bool isBackground = false)
+        {
+            Thread thread = new Thread(new ThreadStart(action));
+            thread.IsBackground = isBackground;
+            thread.Start();
+        }
+    }
+}
